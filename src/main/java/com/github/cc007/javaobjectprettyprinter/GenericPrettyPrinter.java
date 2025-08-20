209f7411f0e867bc187dfba5b376988dee64839c
CC007 <Coolcat_the_best2@Hotmail.com>
2025-08-20 18:53:22 +0200

    Add more overloads and add more javadoc

--- src/main/java/com/github/cc007/javaobjectprettyprinter/GenericPrettyPrinter.java
+++ src/main/java/com/github/cc007/javaobjectprettyprinter/GenericPrettyPrinter.java
@@ -5,10 +5,80 @@ public class GenericPrettyPrinter {
     private GenericPrettyPrinter() {
     }
 
+
+    /**
+     * Pretty prints a Java-style toString() output like: Person[name=Alice, address=Address[street=Main, city=NY]]
+     * <p>
+     * Handles: <br> - Nested brackets <br> - Skips formatting inside string literals <br> - Works with Lombok's default
+     * format: User(username=bob, id=42) <br> - Works with JSON format as well <br> - Works with a mix of the above
+     * <p>
+     * Note: only tested for objects with a toString implementation that prints the contents of the object.
+     * It might break for object references, like when they contain [ characters for array objects
+     *
+     * @param object the object to be pretty-printed
+     * @return the pretty-printed string
+     */
+    public static String prettyPrint(Object object) {
+        return prettyPrint(object.toString());
+    }
+
+    /**
+     * Pretty prints a Java-style toString() output like: Person[name=Alice, address=Address[street=Main, city=NY]]
+     * <p>
+     * Handles: <br> - Nested brackets <br> - Skips formatting inside string literals <br> - Works with Lombok's default
+     * format: User(username=bob, id=42) <br> - Works with JSON format as well <br> - Works with a mix of the above
+     * <p>
+     * Note: only tested for objects with a toString implementation that prints the contents of the object.
+     * It might break for object references, like when they contain [ characters for array objects
+     *
+     * @param object the object to be pretty-printed
+     * @param spaces the number of spaces to use for indents
+     * @return the pretty-printed string
+     */
+    public static String prettyPrint(Object object, int spaces) {
+        return prettyPrint(object.toString(), spaces);
+    }
+
+    /**
+     * Pretty prints a Java-style toString() output like: Person[name=Alice, address=Address[street=Main, city=NY]]
+     * <p>
+     * Handles: <br> - Nested brackets <br> - Skips formatting inside string literals <br> - Works with Lombok's default
+     * format: User(username=bob, id=42) <br> - Works with JSON format as well <br> - Works with a mix of the above
+     * <p>
+     * Note: only tested for objects with a toString implementation that prints the contents of the object.
+     * It might break for object references, like when they contain [ characters for array objects
+     *
+     * @param object the object to be pretty-printed
+     * @param indentationString exact indentation string to use
+     * @return the pretty-printed string
+     */
+    public static String prettyPrint(Object object, String indentationString) {
+        return prettyPrint(object.toString(), indentationString);
+    }
+
+    /**
+     * Pretty prints a Java-style toString() output like: Person[name=Alice, address=Address[street=Main, city=NY]]
+     * <p>
+     * Handles: <br> - Nested brackets <br> - Skips formatting inside string literals <br> - Works with Lombok's default
+     * format: User(username=bob, id=42) <br> - Works with JSON format as well <br> - Works with a mix of the above
+     *
+     * @param input the input string that needs to be pretty-printed
+     * @return the pretty-printed string
+     */
     public static String prettyPrint(String input) {
         return prettyPrint(input, 2);
     }
 
+    /**
+     * Pretty prints a Java-style toString() output like: Person[name=Alice, address=Address[street=Main, city=NY]]
+     * <p>
+     * Handles: <br> - Nested brackets <br> - Skips formatting inside string literals <br> - Works with Lombok's default
+     * format: User(username=bob, id=42) <br> - Works with JSON format as well <br> - Works with a mix of the above
+     *
+     * @param input the input string that needs to be pretty-printed
+     * @param spaces the number of spaces to use for indents
+     * @return the pretty-printed string
+     */
     public static String prettyPrint(String input, int spaces) {
         return prettyPrint(input, " ".repeat(spaces));
     }
@@ -17,9 +87,13 @@ public class GenericPrettyPrinter {
      * Pretty prints a Java-style toString() output like: Person[name=Alice, address=Address[street=Main, city=NY]]
      * <p>
      * Handles: <br> - Nested brackets <br> - Skips formatting inside string literals <br> - Works with Lombok's default
-     * format: User(username=bob, id=42) <br> - Works with JSON format as well - Works with a mix of the above
+     * format: User(username=bob, id=42) <br> - Works with JSON format as well <br> - Works with a mix of the above
+     *
+     * @param input the input string that needs to be pretty-printed
+     * @param indentationString exact indentation string to use
+     * @return the pretty-printed string
      */
-    public static String prettyPrint(String input, String delimiter) {
+    public static String prettyPrint(String input, String indentationString) {
         StringBuilder result = new StringBuilder();
         int indent = 0;
         StringState stringState = new StringState(false, (char) 0);
@@ -37,16 +111,11 @@ public class GenericPrettyPrinter {
             } else if (stringState.insideString) { // If inside a string, do not pretty-print anything
                 result.append(c);
             } else {
-                indent = handleChar(delimiter, c, result, indent);
+                indent = handleChar(indentationString, c, result, indent);
             }
         }
         return result.toString();
     }
-
-    private record StringState(
-            boolean insideString,
-            char stringDelimiter // Can be ' or "
-    ) { }
 
     private static int handleChar(String delimiter, char c, StringBuilder result, int indent) {
         // If it's a space after a comma, do not print at all
@@ -77,4 +146,9 @@ public class GenericPrettyPrinter {
         }
         return indent;
     }
+
+    private record StringState(
+            boolean insideString,
+            char stringDelimiter // Can be ' or "
+    ) { }
 }
